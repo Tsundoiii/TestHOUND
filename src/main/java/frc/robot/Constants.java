f37f86bdd69359ff758a0dad982de50b542427a0
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-03-05 13:00:24 -0500

    add steer constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import com.techhounds.houndutil.houndlib.swerve.CoaxialSwerveModule.SwerveConstants;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class Constants {
     public static final class Drivetrain {
         public static final class FrontLeft {
@@ -48,6 +52,39 @@ public class Constants {
             public static final boolean STEER_ENCODER_INVERTED = false;
 
             public static final double MAGNET_OFFSET = -0.26953125;
+        }
+
+        public static final SwerveConstants SWERVE_CONSTANTS = new SwerveConstants();
+        static {
+            SWERVE_CONSTANTS.DRIVE_kP = 2.9646;
+            SWERVE_CONSTANTS.DRIVE_kI = 0.0;
+            SWERVE_CONSTANTS.DRIVE_kD = 0.0;
+            SWERVE_CONSTANTS.DRIVE_kS = 0.0;
+            SWERVE_CONSTANTS.DRIVE_kV = 2.8024;
+            SWERVE_CONSTANTS.DRIVE_kA = 0.057223;
+            SWERVE_CONSTANTS.STEER_kP = 6.0;
+            SWERVE_CONSTANTS.STEER_kI = 0.0;
+            SWERVE_CONSTANTS.STEER_kD = 0.1;
+
+            SWERVE_CONSTANTS.DRIVE_GEARING = 6.12;
+            SWERVE_CONSTANTS.STEER_GEARING = 150.0 / 7.0;
+            SWERVE_CONSTANTS.WHEEL_CIRCUMFERENCE = 2.0 * Math.PI * 0.0478;
+            SWERVE_CONSTANTS.DRIVE_ENCODER_ROTATIONS_TO_METERS = SWERVE_CONSTANTS.WHEEL_CIRCUMFERENCE
+                    / SWERVE_CONSTANTS.DRIVE_GEARING;
+            SWERVE_CONSTANTS.STEER_ENCODER_ROTATIONS_TO_RADIANS = 2 * Math.PI
+                    / SWERVE_CONSTANTS.STEER_GEARING;
+
+            SWERVE_CONSTANTS.MAX_DRIVING_VELOCITY_METERS_PER_SECOND = 4.282;
+            SWERVE_CONSTANTS.MAX_DRIVING_ACCELERATION_METERS_PER_SECOND_SQUARED = 3;
+            SWERVE_CONSTANTS.MAX_STEER_VELOCITY_RADIANS_PER_SECOND = 8.829 * Math.PI;
+            SWERVE_CONSTANTS.MAX_STEER_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 8.829 * 3 * Math.PI;
+
+            SWERVE_CONSTANTS.DRIVE_CURRENT_LIMIT = 50;
+            SWERVE_CONSTANTS.STEER_CURRENT_LIMIT = 20;
+            SWERVE_CONSTANTS.DRIVE_GEARBOX_REPR = DCMotor.getNEO(1);
+            SWERVE_CONSTANTS.STEER_GEARBOX_REPR = DCMotor.getNEO(1);
+            SWERVE_CONSTANTS.DRIVE_MOI = 0.04;
+            SWERVE_CONSTANTS.STEER_MOI = 0.025;
         }
     }
 }
